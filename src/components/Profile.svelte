4f00099a8db4e182b370d099fc8d4ce4d61a4bc0
Upender Kalwa <gkphv@monsanto.com>
2020-05-15 10:56:17 -0400

    Add About page

---
+++ src/components/Profile.svelte
@@ -0,0 +1,36 @@
+<div class="px-16 py-4 max-w-lg">
+  <!-- IMAGE -->
+  <div class="flex flex-col items-center justify-between text-center">
+    <div class="block h-48 w-48 sm:h-64 sm:w-64 rounded-full overflow-hidden">
+      <img src="/portrait.jpg" alt="Portrait" />
+    </div>
+    <p class="pt-2 text-4xl">Upender Kalwa</p>
+    <p class="text-xl text-gray-800">Researcher - Software Developer</p>
+    <div class="flex justify-between items-center text-gray-800 text-4xl pt-4">
+      <a
+        href="https://scholar.google.com/citations?user=H18-9fkAAAAJ&hl=en&oi=ao"
+        target="_blank"
+        class="px-1"
+      >
+        <i class="ai ai-google-scholar big-icon"></i>
+      </a>
+      <a
+        href="https://www.researchgate.net/profile/Upender_Kalwa"
+        target="_blank"
+        class="px-1"
+      >
+        <i class="ai ai-researchgate big-icon"></i>
+      </a>
+      <a href="https://github.com/ukalwa" target="_blank" class="px-1">
+        <i class="fab fa-github"></i>
+      </a>
+      <a
+        href="https://linkedin.com/in/upender-kalwa"
+        target="_blank"
+        class="px-1"
+      >
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+  </div>
+</div>
